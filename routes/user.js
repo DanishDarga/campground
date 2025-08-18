02cb6632bfa18fc5d192c67bdd0a7392fd7a3b12
DanishDarga <danishdarga@gmail.com>
2025-08-18 23:54:26 +0530

    Added local User Login

--- routes/user.js
+++ routes/user.js
@@ -1,20 +1,41 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
+const passport = require('passport');
+const catchasync = require('../utils/catchasync');
 
 router.get('/register', async (req, res) => {
     res.render('user/register');
 })
 
 router.post('/register', async (req, res) => {
-    const { username, email, password } = req.body;
-    const user = new User({
-        email,
-        username
-    })
-    const registeredUser = await User.register(user, password);
-    console.log("Registered User Successfully");
-    req.flash('success', 'Successfully registered');
-    res.redirect('/campgrounds');
+    try {
+        const { username, email, password } = req.body;
+        const user = new User({ email, username });
+        const registeredUser = await User.register(user, password);
+        console.log("Registered User Successfully");
+        req.flash('success', 'Successfully registered');
+        res.redirect('/campgrounds');
+    } catch (e) {
+        req.flash('error', e.message);
+        res.redirect('/register');
+    }
+});
+
+router.get('/login', (req, res) => {
+    res.render('user/login');
 })
+
+
+router.post('/login',
+    passport.authenticate('local', {
+        failureFlash: true,
+        failureRedirect: '/login'
+    }),
+    (req, res) => {
+        req.flash("success", `Welcome back ${req.user.username}`); // safer: use req.user instead of req.body
+        res.redirect('/campgrounds');
+    }
+);
+
 module.exports = router;
